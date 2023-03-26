7773facad19931dff54520d2c4373f88adbfff49
Gonzalo <gonalc8@gmail.com>
2023-03-26 19:29:44 +0200

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { useFonts } from 'expo-font'
+import { NunitoSans_400Regular } from '@expo-google-fonts/nunito-sans'
+import { PassionOne_400Regular } from '@expo-google-fonts/passion-one'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    NunitoSans: NunitoSans_400Regular,
+    PassionOne: PassionOne_400Regular
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>First commit TS!</Text>
+      <Text style={{ fontFamily: 'PassionOne' }}>First commit TS!</Text>
       <StatusBar style="auto" />
     </View>
   )
