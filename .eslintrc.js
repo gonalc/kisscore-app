fee830bc300cfe13b4ca475cb66d366d405c7bd7
Gonzalo <gonalc8@gmail.com>
2023-03-26 19:08:45 +0200

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,25 @@
 module.exports = {
-	'env': {
-		'es2021': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react/jsx-runtime'
-	],
-	'overrides': [
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react',
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
-}
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+],
+overrides: [],
+parser: "@typescript-eslint/parser",
+parserOptions: {
+	ecmaVersion: "latest",
+    sourceType: "module",
+},
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+    "prettier/prettier": "error",
+  },
+};
