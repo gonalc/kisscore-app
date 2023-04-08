dc93efa1386b8b19825cfe7f20766dc86517aaa2
Gonzalo <gonalc8@gmail.com>
2023-04-08 18:04:55 +0200

    add logout

--- src/views/Leagues/index.tsx
+++ src/views/Leagues/index.tsx
@@ -2,8 +2,17 @@ import { FC, useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { getJWTToken, getStoredUser } from '../../utils/storage'
 import { IUser } from '../../types/users'
+import Button from '../../components/Button'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import type { RootStackParamList } from '../../../App'
+
+type THomeScreenProp = NativeStackNavigationProp<RootStackParamList, 'Leagues'>
 
 const Leagues: FC = () => {
+  const navigation = useNavigation<THomeScreenProp>()
+
   const [token, setToken] = useState<string>('')
   const [user, setUser] = useState<IUser | null>(null)
 
@@ -18,6 +27,12 @@ const Leagues: FC = () => {
 
     getStoredData()
   }, [])
+
+  const logout = async () => {
+    await AsyncStorage.clear()
+
+    navigation.navigate('Login')
+  }
   return (
     <View style={styles.container}>
       <Text>Leagues Screen</Text>
@@ -25,7 +40,9 @@ const Leagues: FC = () => {
       <Text style={{ marginBottom: 50 }}>{token}</Text>
 
       <Text>User:</Text>
-      <Text>{JSON.stringify(user)}</Text>
+      <Text style={{ marginBottom: 50 }}>{JSON.stringify(user)}</Text>
+
+      <Button label="Logout" onPress={logout} />
     </View>
   )
 }
