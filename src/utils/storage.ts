610af268ed9378244869e568017ee5b0737b771f
Gonzalo <gonalc8@gmail.com>
2023-04-08 18:00:15 +0200

    handle stored data

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,2 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { IUser } from '../types/users'
+
 export const JWT_STORAGE_KEY = '@jwt'
 export const USER_STORAGE_KEY = '@user'
+
+export async function getJWTToken(): Promise<string | null> {
+  try {
+    const token = await AsyncStorage.getItem(JWT_STORAGE_KEY)
+
+    return token
+  } catch (error) {
+    console.error('There was some error finding the token: ', error)
+  }
+}
+
+export async function getStoredUser(): Promise<IUser> {
+  try {
+    const jsonUser = await AsyncStorage.getItem(USER_STORAGE_KEY)
+
+    const user = JSON.parse(jsonUser)
+
+    return user
+  } catch (error) {
+    console.error('There was some error finding the user: ', error)
+  }
+}
--- src/views/Leagues/index.tsx
+++ src/views/Leagues/index.tsx
@@ -1,12 +1,40 @@
-import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { FC, useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { getJWTToken, getStoredUser } from '../../utils/storage'
+import { IUser } from '../../types/users'
 
 const Leagues: FC = () => {
+  const [token, setToken] = useState<string>('')
+  const [user, setUser] = useState<IUser | null>(null)
+
+  useEffect(() => {
+    const getStoredData = async () => {
+      const jwt = await getJWTToken()
+      const userData = await getStoredUser()
+
+      setToken(jwt)
+      setUser(userData)
+    }
+
+    getStoredData()
+  }, [])
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Leagues Screen</Text>
+      <Text>Token:</Text>
+      <Text style={{ marginBottom: 50 }}>{token}</Text>
+
+      <Text>User:</Text>
+      <Text>{JSON.stringify(user)}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50
+  }
+})
 
 export default Leagues
