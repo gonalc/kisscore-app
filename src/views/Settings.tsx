6bd666456516caa2099f5ca639f31158f7e35ff9
Gonzalo <gonalc8@gmail.com>
2023-05-26 20:42:47 +0200

    add user data in settings

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -4,11 +4,16 @@ import Button from '../components/Button'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../../App'
+import { useEffect, useState } from 'react'
+import { getStoredUser } from '../utils/storage'
+import { IUser } from '../types/users'
 
 type THomeScreenProp = NativeStackNavigationProp<RootStackParamList, 'LeaguesScreens'>
 
 const Settings = () => {
   const navigation = useNavigation<THomeScreenProp>()
+
+  const [user, setUser] = useState<IUser>()
 
   const logout = async () => {
     await AsyncStorage.clear()
@@ -16,9 +21,23 @@ const Settings = () => {
     navigation.navigate('Login')
   }
 
+  useEffect(() => {
+    const getUser = async () => {
+      const loggedUser = await getStoredUser()
+
+      setUser(loggedUser)
+    }
+
+    getUser()
+  }, [])
+
   return (
     <View>
       <Text>This is the settings screen</Text>
+
+      <Text>Logged as:</Text>
+      <Text>Name: {user?.name}</Text>
+      <Text>ID: {user?.id}</Text>
 
       <Button label="Logout" onPress={logout} />
     </View>
