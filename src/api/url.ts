7784dd640af88f3198f69d608f5c9f94c5eb9a1f
Gonzalo <gonalc8@gmail.com>
2023-06-28 19:06:30 +0200

    setup general stuff

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,6 +1,6 @@
 // Change this to your local IP address
 export const BASE_URL = 'http://192.168.1.150:8080/api'
-// export const BASE_URL = 'http://54.228.82.1200:3000/api'
+// export const BASE_URL = 'http://52.49.189.29:3000/api'
 
 export type APIResponse<T> = {
   data: T
