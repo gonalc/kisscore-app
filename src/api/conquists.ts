084c54b62784879434deba36cba249122b46de56
Gonzalo <gonalc8@gmail.com>
2023-07-13 19:59:03 +0200

    solve api service issues

--- src/api/conquists.ts
+++ src/api/conquists.ts
@@ -1,11 +1,11 @@
-import type { IConquistToCreate } from '../types/conquists'
+import type { IConquist, IConquistToCreate } from '../types/conquists'
 import Api from './apiService'
 
 const api = new Api('conquists')
 
 export async function createConquist(payload: IConquistToCreate) {
   try {
-    const data = await api.post<IConquistToCreate>({ payload })
+    const data = await api.post<IConquist>({ payload })
 
     return data
   } catch (error) {
