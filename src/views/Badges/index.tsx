c361bc5fcf49e94808ae5c6cf1235ea706c10757
Gonzalo <gonalc8@gmail.com>
2023-08-25 19:08:26 +0200

    add explanation modal

--- src/views/Badges/index.tsx
+++ src/views/Badges/index.tsx
@@ -1,19 +1,31 @@
 import Loader from '@components/Loader'
 import useFetchBadges from '@hooks/badges/fetchBadges'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import * as Icons from '@expo/vector-icons'
 import Hexagon from '@components/Hexagon'
 import COLORS from '@utils/colors'
 import { FONT_SIZE, NunitoSans } from '@utils/fonts'
 import i18n from '@i18n/index'
+import Modal from 'react-native-modal'
+import { useState } from 'react'
+import Button from '@components/Button'
+import modalContainer from '@styles/modalContainer'
 
 const Badges = () => {
   const { badges, loading } = useFetchBadges()
+  const [infoModal, setInfoModal] = useState<string | null>(null)
+
+  const { Ionicons } = Icons
 
   return (
     <Loader isLoading={loading}>
       <View style={styles.container}>
-        <Text>Difunde la palabra</Text>
+        <TouchableOpacity onPress={() => setInfoModal('share-app')}>
+          <Text style={styles.groupLabel}>
+            {i18n.t('badges.groups.share-app')}{' '}
+            <Ionicons name="information-circle-outline" size={FONT_SIZE.body} color={COLORS.blue} />
+          </Text>
+        </TouchableOpacity>
         <View style={styles.badgesContainer}>
           {badges.map((badge) => {
             const { name, id, iconFamily, iconKey } = badge
@@ -21,17 +33,29 @@ const Badges = () => {
             const Icon = Icons[iconFamily]
 
             return (
-              <View key={`badge-${name}_${id}`} style={styles.badgeGroup}>
+              <TouchableOpacity
+                onPress={() => alert('Hola!!')}
+                key={`badge-${name}_${id}`}
+                style={styles.badgeGroup}
+              >
                 <Hexagon>
                   <Icon name={iconKey} size={FONT_SIZE.badge} color={COLORS.gray} />
                 </Hexagon>
 
                 <Text style={styles.badgeName}>{i18n.t(`badges.name.${name}`)}</Text>
-              </View>
+              </TouchableOpacity>
             )
           })}
         </View>
       </View>
+
+      <Modal isVisible={!!infoModal} onBackdropPress={() => setInfoModal(null)}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.groupLabel}>{i18n.t(`badges.explanations.${infoModal}`)}</Text>
+
+          <Button label={i18n.t('actions.exit')} onPress={() => setInfoModal(null)} />
+        </View>
+      </Modal>
     </Loader>
   )
 }
@@ -42,7 +66,8 @@ const styles = StyleSheet.create({
   },
   badgesContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    paddingVertical: 10
   },
   badgeGroup: {
     alignItems: 'center'
@@ -51,7 +76,13 @@ const styles = StyleSheet.create({
     color: COLORS.gray,
     fontFamily: NunitoSans,
     fontSize: FONT_SIZE.body
-  }
+  },
+  groupLabel: {
+    color: COLORS.black,
+    fontFamily: NunitoSans,
+    fontSize: FONT_SIZE.body
+  },
+  modalContainer
 })
 
 export default Badges
